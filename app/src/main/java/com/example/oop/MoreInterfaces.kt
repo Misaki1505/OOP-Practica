4d5ea675cf3705747663d1368713b89fba77935c
Misaki1505 <sawako1505@gmail.com>
2023-01-26 19:53:34 -0500

    Changes 26/01/2023

---
+++ app/src/main/java/com/example/oop/MoreInterfaces.kt
@@ -0,0 +1,14 @@
+package com.example.oop
+
+fun main(args: Array<String>) {
+
+}
+
+//A class can implement multiple interfaces
+
+interface Pet{
+    var cutenessLevel: Int
+    fun startPlaying()
+    fun feed()
+}
+
